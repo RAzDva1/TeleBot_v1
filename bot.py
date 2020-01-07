216b159482fcf0f6fc7dea61c79774ba4f0c8f7f
Raz <valentin.goriachev@yandex.ru>
2020-01-07 19:26:26 +0300

    v1.01

--- bot.py
+++ bot.py
@@ -5,14 +5,13 @@ import datetime
 import pytz
 import json
 import traceback
-from telebot import apihelper
 
 
 P_TIMEZONE = pytz.timezone(config.TIMEZONE)
 TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
 
 bot = telebot.TeleBot(config.TOKEN)
-@bot.message_handler(commands=['start'])  
+@bot.message_handler(commands=['start'])
 def start_command(message):  
     bot.send_message(  
         message.chat.id,  
@@ -20,6 +19,12 @@ def start_command(message):
         'To get the exchange rates press /exchange.\n' +  
         'To get help press /help.'  
   )
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+        
+bot.polling(none_stop=True)    

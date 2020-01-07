c16c5fc4e40a228b57482ca1c7daaf193f28bf3c
Raz <valentin.goriachev@yandex.ru>
2020-01-07 18:45:27 +0300

    v1

---
+++ bot.py
@@ -0,0 +1,25 @@
+import telebot
+import config
+import pb
+import datetime
+import pytz
+import json
+import traceback
+from telebot import apihelper
+
+
+P_TIMEZONE = pytz.timezone(config.TIMEZONE)
+TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
+
+bot = telebot.TeleBot(config.TOKEN)
+@bot.message_handler(commands=['start'])  
+def start_command(message):  
+    bot.send_message(  
+        message.chat.id,  
+        'Greetings! I can show you exchange rates.\n' +  
+        'To get the exchange rates press /exchange.\n' +  
+        'To get help press /help.'  
+  )
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
+
---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = '1001612814:AAFRJYvo-1Ektv-u871R7GkRQF_4A0EpBfA'  # заменить на токен своего бота
+API_URL = 'https://telegg.ru/orig/bot'
+TIMEZONE = 'Europe/Moscow'
+TIMEZONE_COMMON_NAME = 'Moscow'
